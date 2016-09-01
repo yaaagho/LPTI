f4fc32c409fcf23c27df2f79a152a985c95ad148
cacocacaio <ccnsassino1998@hotmail.com>
2016-09-01 12:07:15 -0300

    imposto é robô

--- bootstrap/js/meuscript.js
+++ bootstrap/js/meuscript.js
@@ -1,45 +1,29 @@
 $(document).ready(function(e){
-	
-	$('#buten').click (function(e){
+
+	$('a').not('#buten').click (function(e){
 		
 		e.preventDefault();
 		var href = $(this) . attr('href');
-		$('#painel').load(href + ' #painel', function(){
-		
-			$('a').click (function(e){
-				
-				e.preventDefault();
-				var href = $(this) . attr('href');
-				$('#conteudo').load(href + ' #conteudo', function(){
-					
-						$('a').click (function(e){
-							
-							e.preventDefault();
-							var href = $(this) . attr('href');
-							$('#conteudo').load(href + '#conteudo');
-						});
-				});
-			});
-		});
+		$('#conteudo').load(href + ' #conteudo > *');
 	});
 	
-//	$('.col-md-9').hide();
-//	$('.col-md-9').css("position", "absolute");
-//	$('.col-md-9').css("margin", "auto");
-//	$('.col-md-9').css("left", "70px");
-//	
-//	$('.row').hover(
-//	
-//		function(){
-//		
-//			$(this).children().show();
-//		},
-//		
-//		function(){
-//			
-//			$('.col-md-9').hide();
-//		}
-//	);
+	$('.col-md-9').hide();
+	$('.col-md-9').css("position", "absolute");
+	$('.col-md-9').css("margin", "auto");
+	$('.col-md-9').css("left", "70px");
+	
+	$('.row').hover(
+	
+		function(){
+		
+			$(this).children().show();
+		},
+		
+		function(){
+			
+			$('.col-md-9').hide();
+		}
+	);
 });
 
 function ajax(){
